98fdbf6c4178811dd909ffefe48816d1bd7f8b88
Vaishnavi7397 <145182915+Vaishnavi7397@users.noreply.github.com>
2026-05-22 21:04:04 +0530

    Add files via upload

---
+++ sum_and_mean.py
@@ -0,0 +1,9 @@
+# Please write a program which asks the user for four numbers. The program then prints out the sum and the mean of the numbers.
+
+num1 = int(input("Number 1:"))
+num2 = int(input("Number 2:"))
+num3 = int(input("Number 3:"))
+num4 = int(input("Number 4:"))
+sum = num1 + num2 + num3 + num4
+mean = sum / 4
+print(f"The sum of the numbers is {sum} and the mean is {mean}")
